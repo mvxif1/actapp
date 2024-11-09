339665fc65690e127fb023ed09081a66b263a957
mvxif1 <m.ferrer@duocuc.cl>
2024-11-09 02:40:16 -0300

    CAMBIOS CASI FINALES
    Se ve todo listo, solamente AJUSTAR BOTON DE cerrarTicket().
    Solamente copiar lo que sale en cerrarTicketP(), y hacer algunas pruebas de error

--- src/app/pages/despachoform/despachoform.page.ts
+++ src/app/pages/despachoform/despachoform.page.ts
@@ -28,23 +28,31 @@ export class DespachoformPage implements OnInit {
   username: string = ''; 
   password: string = '';
   base64Image: string | ArrayBuffer | null = null;
+  //carga por 10 tickets
+  displayGuias: Guia[] = [];
+  numGuiasCarga: number = 10;
+  currentBatchIndex: number = 0; //indica indice actual para cargar mas guias
+  infiniteScrollDisabled: boolean = false;
 
   //filtro por id
   filtroTicketArray: Guia[] = [];
   searchTerm: string = '';
   selectedGuia: Guia | null = null;
   selectedTicket: Ticket | null = null;
-  
+
+  //mostrar contenidos de botones
+  mostrarDetalle = false;
   mostrarContenido = false;
+
   //carga (SPINNER)
   isLoading: boolean = false; 
+
   //carga de fotos
   photos: string[] = [];
   loadingImage!: boolean;
+
   constructor(
     private camera: Camera, 
-    private formBuilder: FormBuilder, 
-    private http: HttpClient, 
     private api: ApiService,
     private db: DbService,
     private internet: Network,
@@ -58,27 +66,40 @@ export class DespachoformPage implements OnInit {
   }
   
   refreshTickets(event: any) {
+    this.selectedGuia = null;
+    this.displayGuias = this.guiaArray;
+    this.infiniteScrollDisabled = false;
+    this.mostrarContenido = false;
+    this.mostrarDetalle = false;
     this.fetchTickets();
     event.target.complete();
+  }
+
+  //carga tickets al hacer scroll
+  loadMoreTickets(event: any) {
+    const nextBatchIndex = this.currentBatchIndex + this.numGuiasCarga;
+    const newTickets = this.filtroTicketArray.slice(this.currentBatchIndex, nextBatchIndex);
+    this.displayGuias = [...this.displayGuias, ...newTickets];
+    this.currentBatchIndex = nextBatchIndex;
+  
+    event.target.complete();
+  
+    // Desactivar el scroll si todos los tickets están cargados
+    if (this.displayGuias.length >= this.filtroTicketArray.length) {
+      event.target.disabled = true;
+    }
   }
 
   fetchTickets() {
     this.isLoading = true;
     this.api.getListTickets(this.username, this.password).subscribe({
       next: (response) => {
-        const guiaMap: { [guiaId: string]: Guia } = {};
-
-        // Agrupar tickets por guía
-        response.tickets.forEach((ticket: Ticket) => {
-          if (!guiaMap[ticket.guia!]) {
-            guiaMap[ticket.guia!] = { guia: ticket.guia!, ticket: [] };
-          }
-          guiaMap[ticket.guia!].ticket.push(ticket);
-        });
-
-        // Convertir el mapa en un array de guías
-        this.guiaArray = Object.values(guiaMap);
-        this.isLoading = false; 
+        console.log(response);
+        this.guiaArray = response.tickets || [];
+        this.guiaArray.shift();
+        this.filtroTicketArray = this.guiaArray;
+        this.displayGuias = this.filtroTicketArray.slice(0, this.numGuiasCarga);
+        this.isLoading = false
       },
       error: (error) => {
         console.error('Error al obtener los tickets:', error);
@@ -89,47 +110,76 @@ export class DespachoformPage implements OnInit {
   
 
   filtrarTicket(event: any) {
+    console.log('Evento de búsqueda:', event.target.value);  // Verifica el valor del input
     this.isLoading = true;
-    this.cdr.detectChanges();  // Forzar la detección de cambios
+    this.selectedGuia= null;
+    this.cdr.detectChanges();
   
-    const query = event.target.value.toLowerCase(); 
-    this.filtroTicketArray = this.guiaArray.filter(ticket => 
-      ticket.guia.toLowerCase().includes(query)
-    );
+    const query = event.target.value?.toLowerCase() || '';
+    console.log('Consulta de búsqueda:', query);  // Verifica la cadena de búsqueda
+  
+    if (query === '') {
+      this.filtroTicketArray = [...this.guiaArray];
+      this.displayGuias = [...this.guiaArray];
+    } else {
+      this.filtroTicketArray = this.guiaArray.filter(guia => 
+        guia.guia.toLowerCase().includes(query)
+      );
+      this.displayGuias = this.guiaArray.filter(guia => 
+        guia.guia.toLowerCase().includes(query)
+      );
+    }
   
     setTimeout(() => {
       this.isLoading = false;
-      this.cdr.detectChanges();  // Forzar la detección de cambios
+      this.cdr.detectChanges();
     }, 500);
   }
+  
+  
   onGuiaSelect(guiaId: string) {
     this.selectedGuia = this.guiaArray.find(guia => guia.guia === guiaId) || null;
-
+    this.infiniteScrollDisabled = true;
     if (this.selectedGuia) {
-      this.filtroTicketArray = [];
+      this.displayGuias = [];
     }
   }
   
   onTicketSelect(ticketId: string) {
-    this.selectedTicket = this.selectedGuia?.ticket.find(ticket => ticket.id === ticketId) || null;
-
+    if (this.selectedTicket && this.selectedTicket.id === ticketId) {
+      this.mostrarDetalle = !this.mostrarDetalle;
+    } else {
+      this.selectedTicket = this.selectedGuia?.ticket.find(t => t.id === ticketId) || null;
+      this.mostrarDetalle = true;
+    }
   }
+  
+  
 
   volverListTicket() {
-    this.selectedTicket = null;
-    this.filtroTicketArray = this.guiaArray;
+    this.isLoading = true;
+    
+    setTimeout(() => {
+      this.selectedGuia = null;
+      this.displayGuias = this.guiaArray;
+      this.isLoading = false;
+      this.mostrarContenido = false;
+      this.mostrarDetalle = false;
+      this.infiniteScrollDisabled = false;
+    }, 500);
   }
+  
 
-  despliegaContenido() {
+  despliegaContenido(){
     this.mostrarContenido = !this.mostrarContenido;
   }
-
   decodeHtml(html: string): string {
     const txt = document.createElement('textarea');
     txt.innerHTML = html;
     return txt.value;
   }
 
+  //subida de archivo
   onFileSelected(event: Event, ticket: Ticket) {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length > 0) {
@@ -267,10 +317,10 @@ export class DespachoformPage implements OnInit {
 
   //PRUEBA
   async cerrarTicketP() {
-    // Confirmación antes de cerrar el ticket
+    // Confirmación antes de cerrar los tickets
     const confirmacion = await this.db.presentAlertConfirm(
-      '¿Estás seguro que quieres cerrar el ticket?',
-      'Sí, cerrar ticket',
+      '¿Estás seguro que quieres cerrar todos los tickets de esta guía?',
+      'Sí, cerrar tickets',
       'Cancelar'
     );
   
@@ -278,64 +328,52 @@ export class DespachoformPage implements OnInit {
       const inputFile = document.createElement('input');
       inputFile.type = 'file';
       inputFile.accept = '.jpg, .jpeg, .png';
+  
       inputFile.onchange = async (event: Event) => {
         const target = event.target as HTMLInputElement;
         if (target?.files?.length) {
           const file = target.files[0];
-          
-          //console.log('Archivo seleccionado:');
-          //console.log('Nombre del archivo:', file.name);
-          //console.log('Tamaño del archivo:', file.size);
-          //console.log('Tipo de archivo:', file.type);
-  
-          // Leer el archivo seleccionado como Base64
           const reader = new FileReader();
+  
           reader.onloadend = async () => {
             const base64File = reader.result as string;
-
-            // Depurar la cadena Base64
-            //console.log('Base64 del archivo:', base64File);
+            const base64Image = base64File.split(',')[1];
   
-            // Eliminar el prefijo data:image/jpeg;base64, si existe
-            const base64Image = base64File.split(',')[1]; // Elimina el prefijo si es necesario
-            //console.log('Base64 limpio:', base64Image);
+            // Verifica si hay una guía seleccionada y que tiene tickets
+            if (this.selectedGuia && this.selectedGuia.ticket.length > 0) {
+              // Iterar sobre cada ticket de la guía seleccionada
+              for (const ticket of this.selectedGuia.ticket) {
+                await this.api.cerrarTicket(this.username, this.password, ticket.id, file.name, base64Image).subscribe({
+                  next: (response) => {
+                    console.log(`Respuesta de la API para ticket ${ticket.id}: `, response);
+                  },
+                  error: (error) => {
+                    console.error(`Error al cerrar el ticket ${ticket.id}:`, error);
+                    this.db.presentAlertN(`Error al cerrar el ticket ${ticket.id}. Intenta de nuevo.`);
+                  }
+                });
+              }
   
-            // Verificar si tenemos un ticket seleccionado
-            if (this.selectedTicket) {
-              // Depurar los datos que se enviarán a la API
-              //console.log('Enviando datos a la API:');
-              //console.log('ID del Ticket:', this.selectedTicket.id);
-              //console.log('Nombre del archivo:', file.name);
-              //console.log('Base64 del archivo:', base64Image);
-  
-              // Llamar a la API para cerrar el ticket
-              this.api.cerrarTicket(this.username, this.password, this.selectedTicket.id, file.name,  base64Image
-              ).subscribe({
-                next: (response) => {
-                  console.log("Respuesta de la API: ", response);
-                },
-                error: (error) => {
-                  // Si la API devuelve un error, mostramos un mensaje de error
-                  console.error('Error al cerrar el ticket:', error);
-                  this.db.presentAlertN('Error al cerrar el ticket. Intenta de nuevo.');
-                }
-              });
+              // Mensaje de éxito después de cerrar todos los tickets
+              this.db.presentAlertP(`Todos los tickets de la guía ${this.selectedGuia.guia} han sido cerrados exitosamente.`);
+              this.displayGuias = this.guiaArray; // Actualizar la lista
             } else {
-              this.db.presentAlertN('No se ha seleccionado un ticket.');
+              this.db.presentAlertN('No se ha seleccionado una guía válida.');
             }
           };
+  
           reader.readAsDataURL(file); // Convertir el archivo a base64
         } else {
           this.db.presentAlertN('No se seleccionó ningún archivo');
         }
       };
   
-      // Abrir el selector de archivos
       inputFile.click();
     } else {
       this.db.presentAlertN('Operación cancelada');
     }
   }
+  
 
   
 }
